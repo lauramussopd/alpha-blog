6df24ba4a94aadefa7212b25b2fc8a1235650bff
Laura Musso <lauramussopd@gmail.com>
2026-04-25 14:16:03 +0200

    Actions added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,43 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new # get
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create # post
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created." # Aggiunge un messaggio flash per confermare la creazione
+      redirect_to @article # Reindirizza alla pagina del nuovo articolo appena creato
+    else
+      render "new", status: :unprocessable_entity # Se c'è un errore, ricarica il form (con gli errori salvati)
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated." # Aggiunge un messaggio flash per confermare l'aggiornamento
+      redirect_to @article # Reindirizza alla pagina del articolo aggiornato
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted." # Aggiunge un messaggio flash per confermare la cancellazione
+    redirect_to articles_path # Reindirizza alla lista degli articoli dopo la cancellazione
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
